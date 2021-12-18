4aad657af60464013628d529b7dbce94b7c4fbfb
Mattia2700 <32057853+Mattia2700@users.noreply.github.com>
2021-12-18 16:46:00 +0100

    chore: improve robot control structure + fix blender world (#4)

--- src/f_controller/include/f_controller/m_kinematics.hpp
+++ src/f_controller/include/f_controller/m_kinematics.hpp
@@ -4,22 +4,24 @@
 #include <eigen3/Eigen/Core>
 #include <eigen3/Eigen/Dense>
 #include <eigen3/Eigen/Householder>
+#include <vector>
 
 class KIN {
 public:
-  KIN();
-  ~KIN();
+  static const Eigen::Matrix3d eul2rotm(const Eigen::Vector3d &eul);                                                           // convert euler angles to rotation matrix
+  static const Eigen::Vector3d rotm2eul(const Eigen::Matrix3d &rotm);                                                          // convert rotation matrix to euler angles
+  static const Eigen::Matrix4d create_homogeneous_matrix(const Eigen::Vector3d &position, const Eigen::Matrix3d &orientation); // create homogeneous matrix from position and orientation
 
-  static const Eigen::MatrixXd ik(const Eigen::Matrix4d &T60);                      // get joint angles from end-effector position and orientation
-  static const Eigen::Matrix4d fk(const Eigen::VectorXd &theta);                    // get homogeneous matrix from forward kinematics
   static const Eigen::Vector3d get_position(const Eigen::Matrix4d &homo_matrix);    // get end-effector position using homogeneous matrix
   static const Eigen::Matrix3d get_orientation(const Eigen::Matrix4d &homo_matrix); // get end-effector orientation using homogeneouse matrix
 
-  static const Eigen::MatrixXd p2p(const Eigen::VectorXd &qEs, const Eigen::Vector3d &xEf, const Eigen::Vector3d &phiEf); // get trajectory in order to reach end-effector position and orientation
+  static const Eigen::MatrixXd p2p(const Eigen::VectorXd &qEs, const Eigen::VectorXd &qEf); // get trajectory in order to reach end-effector position and orientation
 
-  static const Eigen::Matrix3d eul2rotm(const Eigen::Vector3d &eul);                                                         // convert euler angles to rotation matrix
-  static const Eigen::Vector3d rotm2eul(const Eigen::Matrix3d &rotm);                                                        // convert rotation matrix to euler angles
-  static const Eigen::Matrix4d createHomogeneousMatrix(const Eigen::Vector3d &position, const Eigen::Matrix3d &orientation); // create homogeneous matrix from position and orientation
+  static const Eigen::MatrixXd ik(const Eigen::Matrix4d &T60);   // get joint angles from end-effector position and orientation
+  static const Eigen::Matrix4d fk(const Eigen::VectorXd &theta); // get homogeneous matrix from forward kinematics
+
+  // temp
+  static const Eigen::VectorXd best_angles(const Eigen::VectorXd &actual, const Eigen::MatrixXd &possible); // get best angles for p2p trajectory
 
 private:
   static constexpr double minT = 0.0;
@@ -30,7 +32,7 @@ private:
   static const Eigen::VectorXd d;     // link offset
   static const Eigen::VectorXd alpha; // link twist
 
-  static const Eigen::Matrix4d computeMatrix(double theta, short frame); // return matrix based on theta and frame
-  static const bool checkRow(const Eigen::VectorXd &vec); // check if matrix contains NaN values
-  static const Eigen::VectorXd bestAngles(const Eigen::VectorXd &actual, const Eigen::MatrixXd &possible);
+private:
+  static const Eigen::Matrix4d compute_matrix(double theta, short frame); // return matrix based on theta and frame
+  static const bool check_row(const Eigen::VectorXd &vec);                // check if matrix contains NaN values
 };
--- src/f_controller/src/m_kinematics.cpp
+++ src/f_controller/src/m_kinematics.cpp
@@ -1,14 +1,57 @@
 #include "f_controller/m_kinematics.hpp"
-#include <iostream>
-#include <vector>
+
+constexpr double KIN::minT;
+constexpr double KIN::maxT;
+constexpr double KIN::step;
 
 const Eigen::VectorXd KIN::a = (Eigen::VectorXd(6) << 0, -0.425, -0.39225, 0, 0, 0).finished();
 const Eigen::VectorXd KIN::d = (Eigen::VectorXd(6) << 0.089159, 0, 0, 0.10915, 0.09465, 0.0823).finished();
 const Eigen::VectorXd KIN::alpha = (Eigen::VectorXd(6) << 0, M_PI_2, 0, 0, M_PI_2, -M_PI_2).finished();
 
-constexpr double KIN::minT;
-constexpr double KIN::maxT;
-constexpr double KIN::step;
+const Eigen::Matrix4d KIN::compute_matrix(double theta, short frame) {
+  return (Eigen::Matrix4d() << cos(theta), -sin(theta), 0, a((((frame - 1) % 6) + 6) % 6),
+          sin(theta) * cos(alpha(frame)), cos(theta) * cos(alpha(frame)), -sin(alpha(frame)), -sin(alpha(frame)) * d(frame),
+          sin(theta) * sin(alpha(frame)), cos(theta) * sin(alpha(frame)), cos(alpha(frame)), cos(alpha(frame)) * d(frame),
+          0, 0, 0, 1)
+      .finished();
+}
+
+const bool KIN::check_row(const Eigen::VectorXd &vec) {
+  bool validRow = true;
+  for (int i = 0; i < vec.rows() && validRow; i++) {
+    if (std::isnan(vec(i))) {
+      validRow = false;
+    }
+  }
+  return validRow;
+}
+
+const Eigen::VectorXd KIN::best_angles(const Eigen::VectorXd &actual, const Eigen::MatrixXd &possible) {
+  // add angle choosing based on joint limits?
+  std::vector<std::pair<double, short>> diffs;
+  std::vector<short> validRows;
+  for (int i = 0; i < possible.rows(); i++) {
+    if (check_row(possible.row(i).transpose())) {
+      validRows.push_back(i);
+    }
+  }
+
+  for (int i = 0; i < possible.cols() && validRows.size() != 1; i++) {
+    for (int j = 0; j < possible.rows(); j++) {
+      if (std::find(validRows.begin(), validRows.end(), j) != validRows.end()) {
+        diffs.push_back(std::make_pair(std::abs(actual(i) - possible(j, i)), j));
+      }
+    }
+    std::sort(diffs.begin(), diffs.end());
+    validRows.resize(0);
+    validRows.push_back(diffs[0].second);
+    for (int k = 1; k < diffs.size() && diffs[k].first == diffs[0].first; k++) {
+      validRows.push_back(diffs[k].second);
+    }
+    diffs.resize(0);
+  }
+  return possible.row(validRows[0]).transpose();
+}
 
 const Eigen::Matrix3d KIN::eul2rotm(const Eigen::Vector3d &eul) {
   return (Eigen::Matrix3d() << cos(eul(0)) * cos(eul(1)), cos(eul(0)) * sin(eul(1)) * sin(eul(2)) - sin(eul(0)) * cos(eul(2)), cos(eul(0)) * sin(eul(1)) * cos(eul(2)) + sin(eul(0)) * sin(eul(2)),
@@ -21,67 +64,26 @@ const Eigen::Vector3d KIN::rotm2eul(const Eigen::Matrix3d &rotm) {
   return rotm.eulerAngles(2, 1, 0);
 }
 
-const Eigen::Matrix4d KIN::computeMatrix(double theta, short frame) {
-  return (Eigen::Matrix4d() << cos(theta), -sin(theta), 0, a((((frame-1)%6)+6)%6),
-          sin(theta) * cos(alpha(frame)), cos(theta) * cos(alpha(frame)), -sin(alpha(frame)), -sin(alpha(frame)) * d(frame),
-          sin(theta) * sin(alpha(frame)), cos(theta) * sin(alpha(frame)), cos(alpha(frame)), cos(alpha(frame)) * d(frame),
+const Eigen::Matrix4d KIN::create_homogeneous_matrix(const Eigen::Vector3d &position, const Eigen::Matrix3d &orientation) {
+  return (Eigen::Matrix4d() << orientation(0, 0), orientation(0, 1), orientation(0, 2), position(0),
+          orientation(1, 0), orientation(1, 1), orientation(1, 2), position(1),
+          orientation(2, 0), orientation(2, 1), orientation(2, 2), position(2),
           0, 0, 0, 1)
       .finished();
 }
 
-const bool KIN::checkRow(const Eigen::VectorXd &vec) {
-  bool validRow = true;
-  for (int i = 0; i < vec.rows() && validRow; i++) {
-      if (std::isnan(vec(i))) {
-        validRow = false;
-      }
-  }
-  return validRow;
+const Eigen::Vector3d KIN::get_position(const Eigen::Matrix4d &homo_matrix) {
+  return (Eigen::Vector3d() << homo_matrix(0, 3), homo_matrix(1, 3), homo_matrix(2, 3)).finished();
 }
 
-const Eigen::VectorXd KIN::bestAngles(const Eigen::VectorXd &actual, const Eigen::MatrixXd &possible) {
-
-  std::vector<std::pair<double, short>> diffs;
-  std::vector<short> validRows;
-  for (int i = 0; i < possible.rows(); i++) {
-    if (checkRow(possible.row(i).transpose())) {
-      validRows.push_back(i);
-    }
-  }
-
-  for(int i=0; i<possible.cols() && validRows.size()!=1; i++){
-    for(int j=0; j<possible.rows(); j++){
-      if(std::find(validRows.begin(), validRows.end(), j) != validRows.end()){
-        diffs.push_back(std::make_pair(std::abs(actual(i)-possible(j,i)), j));
-      }
-    }
-    std::sort(diffs.begin(), diffs.end());
-    validRows.resize(0);
-    validRows.push_back(diffs[0].second);
-    for(int k=1; k<diffs.size() && diffs[k].first == diffs[0].first; k++){
-      validRows.push_back(diffs[k].second);
-    }
-    /*std::cout << i+1 << "° angles: ";
-    for(short r: validRows){
-      std::cout << r << "\t";
-    }
-    std::cout << std::endl;*/
-    diffs.resize(0);
-  }
-
-  //std::cout << "Chosed row " << validRows[0] << std::endl;
-  return possible.row(validRows[0]).transpose();
+const Eigen::Matrix3d KIN::get_orientation(const Eigen::Matrix4d &homo_matrix) {
+  return (Eigen::Matrix3d() << homo_matrix(0, 0), homo_matrix(0, 1), homo_matrix(0, 2),
+          homo_matrix(1, 0), homo_matrix(1, 1), homo_matrix(1, 2),
+          homo_matrix(2, 0), homo_matrix(2, 1), homo_matrix(2, 2))
+      .finished();
 }
 
-const Eigen::MatrixXd KIN::p2p(const Eigen::VectorXd &qEs, const Eigen::Vector3d &xEf, const Eigen::Vector3d &phiEf) {
-
-  Eigen::MatrixXd thetas=ik(createHomogeneousMatrix(xEf, eul2rotm(phiEf)));
-  //std::cout << "Actual angles: " << qEs.transpose() << std::endl;
-  //std::cout << "Possible angles:" << std::endl << thetas << std::endl;
-  Eigen::VectorXd qEf = bestAngles(qEs,thetas);
-  //std::cout << "Chosed angles: " << qEf.transpose() << std::endl;
-  //std::cout << "qEf: " << qEf.transpose() << std::endl;
-  //std::cout << "Goal:\t" << KIN::get_position(fk(qEf)).transpose() << std::endl;
+const Eigen::MatrixXd KIN::p2p(const Eigen::VectorXd &qEs, const Eigen::VectorXd &qEf) {
 
   Eigen::MatrixXd A(6, 6);
 
@@ -107,43 +109,22 @@ const Eigen::MatrixXd KIN::p2p(const Eigen::VectorXd &qEs, const Eigen::Vector3d
   int rowIndex = 0;
 
   for (double t = minT; t <= maxT; t += step, rowIndex++) {
-    ////std::cout << "t: " << t << std::endl;
     for (int i = 0; i < qEs.size(); i++) {
       Th(rowIndex, i) = A(i, 0) + A(i, 1) * t + A(i, 2) * t * t + A(i, 3) * t * t * t + A(i, 4) * t * t * t * t + A(i, 5) * t * t * t * t * t;
     }
   }
 
-  //std::cout << "End: " << get_position(fk(Th.row(Th.rows()-1).transpose())).transpose() << std::endl;
   return Th;
-}
-
-const Eigen::Vector3d KIN::get_position(const Eigen::Matrix4d &homo_matrix) {
-  return (Eigen::Vector3d() << homo_matrix(0, 3), homo_matrix(1, 3), homo_matrix(2, 3)).finished();
-}
-
-const Eigen::Matrix3d KIN::get_orientation(const Eigen::Matrix4d &homo_matrix) {
-  return (Eigen::Matrix3d() << homo_matrix(0, 0), homo_matrix(0, 1), homo_matrix(0, 2),
-          homo_matrix(1, 0), homo_matrix(1, 1), homo_matrix(1, 2),
-          homo_matrix(2, 0), homo_matrix(2, 1), homo_matrix(2, 2))
-      .finished();
-}
-
-const Eigen::Matrix4d KIN::createHomogeneousMatrix(const Eigen::Vector3d &position, const Eigen::Matrix3d &orientation) {
-  return (Eigen::Matrix4d() << orientation(0, 0), orientation(0, 1), orientation(0, 2), position(0),
-          orientation(1, 0), orientation(1, 1), orientation(1, 2), position(1),
-          orientation(2, 0), orientation(2, 1), orientation(2, 2), position(2),
-          0, 0, 0, 1)
-      .finished();
 }
 
 const Eigen::Matrix4d KIN::fk(const Eigen::VectorXd &theta) {
 
-  Eigen::Matrix4d T10 = computeMatrix(theta(0), 0);
-  Eigen::Matrix4d T21 = computeMatrix(theta(1), 1);
-  Eigen::Matrix4d T32 = computeMatrix(theta(2), 2);
-  Eigen::Matrix4d T43 = computeMatrix(theta(3), 3);
-  Eigen::Matrix4d T54 = computeMatrix(theta(4), 4);
-  Eigen::Matrix4d T65 = computeMatrix(theta(5), 5);
+  Eigen::Matrix4d T10 = compute_matrix(theta(0), 0);
+  Eigen::Matrix4d T21 = compute_matrix(theta(1), 1);
+  Eigen::Matrix4d T32 = compute_matrix(theta(2), 2);
+  Eigen::Matrix4d T43 = compute_matrix(theta(3), 3);
+  Eigen::Matrix4d T54 = compute_matrix(theta(4), 4);
+  Eigen::Matrix4d T65 = compute_matrix(theta(5), 5);
 
   Eigen::Matrix4d T06 = T10 * T21 * T32 * T43 * T54 * T65;
 
@@ -187,19 +168,19 @@ const Eigen::MatrixXd KIN::ik(const Eigen::Matrix4d &T60) {
 
   Eigen::Matrix4d T41m;
 
-  T41m = computeMatrix(th1_1, 0).inverse() * T60 * computeMatrix(th6_1, 5).inverse() * computeMatrix(th5_1, 4).inverse();
+  T41m = compute_matrix(th1_1, 0).inverse() * T60 * compute_matrix(th6_1, 5).inverse() * compute_matrix(th5_1, 4).inverse();
   Eigen::Vector3d p41_1 = (Eigen::Vector3d() << T41m(0, 3), T41m(1, 3), T41m(2, 3)).finished();
   double p41xz_1 = hypot(p41_1(0), p41_1(2));
 
-  T41m = computeMatrix(th1_1, 0).inverse() * T60 * computeMatrix(th6_2, 5).inverse() * computeMatrix(th5_2, 4).inverse();
+  T41m = compute_matrix(th1_1, 0).inverse() * T60 * compute_matrix(th6_2, 5).inverse() * compute_matrix(th5_2, 4).inverse();
   Eigen::Vector3d p41_2 = (Eigen::Vector3d() << T41m(0, 3), T41m(1, 3), T41m(2, 3)).finished();
   double p41xz_2 = hypot(p41_2(0), p41_2(2));
 
-  T41m = computeMatrix(th1_2, 0).inverse() * T60 * computeMatrix(th6_3, 5).inverse() * computeMatrix(th5_3, 4).inverse();
+  T41m = compute_matrix(th1_2, 0).inverse() * T60 * compute_matrix(th6_3, 5).inverse() * compute_matrix(th5_3, 4).inverse();
   Eigen::Vector3d p41_3 = (Eigen::Vector3d() << T41m(0, 3), T41m(1, 3), T41m(2, 3)).finished();
   double p41xz_3 = hypot(p41_3(0), p41_3(2));
 
-  T41m = computeMatrix(th1_2, 0).inverse() * T60 * computeMatrix(th6_4, 5).inverse() * computeMatrix(th5_4, 4).inverse();
+  T41m = compute_matrix(th1_2, 0).inverse() * T60 * compute_matrix(th6_4, 5).inverse() * compute_matrix(th5_4, 4).inverse();
   Eigen::Vector3d p41_4 = (Eigen::Vector3d() << T41m(0, 3), T41m(1, 3), T41m(2, 3)).finished();
   double p41xz_4 = hypot(p41_4(0), p41_4(2));
 
@@ -227,35 +208,35 @@ const Eigen::MatrixXd KIN::ik(const Eigen::Matrix4d &T60) {
   Eigen::Matrix4d T43m;
   Eigen::Vector3d xhat43;
 
-  T43m = computeMatrix(th3_1, 2).inverse() * computeMatrix(th2_1, 1).inverse() * computeMatrix(th1_1, 0).inverse() * T60 * computeMatrix(th6_1, 5).inverse() * computeMatrix(th5_1, 4).inverse();
+  T43m = compute_matrix(th3_1, 2).inverse() * compute_matrix(th2_1, 1).inverse() * compute_matrix(th1_1, 0).inverse() * T60 * compute_matrix(th6_1, 5).inverse() * compute_matrix(th5_1, 4).inverse();
   xhat43 << T43m(0, 0), T43m(1, 0), T43m(2, 0);
   double th4_1 = std::real(atan2(xhat43(1), xhat43(0)));
 
-  T43m = computeMatrix(th3_2, 2).inverse() * computeMatrix(th2_2, 1).inverse() * computeMatrix(th1_1, 0).inverse() * T60 * computeMatrix(th6_2, 5).inverse() * computeMatrix(th5_2, 4).inverse();
+  T43m = compute_matrix(th3_2, 2).inverse() * compute_matrix(th2_2, 1).inverse() * compute_matrix(th1_1, 0).inverse() * T60 * compute_matrix(th6_2, 5).inverse() * compute_matrix(th5_2, 4).inverse();
   xhat43 << T43m(0, 0), T43m(1, 0), T43m(2, 0);
   double th4_2 = std::real(atan2(xhat43(1), xhat43(0)));
 
-  T43m = computeMatrix(th3_3, 2).inverse() * computeMatrix(th2_3, 1).inverse() * computeMatrix(th1_2, 0).inverse() * T60 * computeMatrix(th6_3, 5).inverse() * computeMatrix(th5_3, 4).inverse();
+  T43m = compute_matrix(th3_3, 2).inverse() * compute_matrix(th2_3, 1).inverse() * compute_matrix(th1_2, 0).inverse() * T60 * compute_matrix(th6_3, 5).inverse() * compute_matrix(th5_3, 4).inverse();
   xhat43 << T43m(0, 0), T43m(1, 0), T43m(2, 0);
   double th4_3 = std::real(atan2(xhat43(1), xhat43(0)));
 
-  T43m = computeMatrix(th3_4, 2).inverse() * computeMatrix(th2_4, 1).inverse() * computeMatrix(th1_2, 0).inverse() * T60 * computeMatrix(th6_4, 5).inverse() * computeMatrix(th5_4, 4).inverse();
+  T43m = compute_matrix(th3_4, 2).inverse() * compute_matrix(th2_4, 1).inverse() * compute_matrix(th1_2, 0).inverse() * T60 * compute_matrix(th6_4, 5).inverse() * compute_matrix(th5_4, 4).inverse();
   xhat43 << T43m(0, 0), T43m(1, 0), T43m(2, 0);
   double th4_4 = std::real(atan2(xhat43(1), xhat43(0)));
 
-  T43m = computeMatrix(th3_5, 2).inverse() * computeMatrix(th2_5, 1).inverse() * computeMatrix(th1_1, 0).inverse() * T60 * computeMatrix(th6_1, 5).inverse() * computeMatrix(th5_1, 4).inverse();
+  T43m = compute_matrix(th3_5, 2).inverse() * compute_matrix(th2_5, 1).inverse() * compute_matrix(th1_1, 0).inverse() * T60 * compute_matrix(th6_1, 5).inverse() * compute_matrix(th5_1, 4).inverse();
   xhat43 << T43m(0, 0), T43m(1, 0), T43m(2, 0);
   double th4_5 = std::real(atan2(xhat43(1), xhat43(0)));
 
-  T43m = computeMatrix(th3_6, 2).inverse() * computeMatrix(th2_6, 1).inverse() * computeMatrix(th1_1, 0).inverse() * T60 * computeMatrix(th6_2, 5).inverse() * computeMatrix(th5_2, 4).inverse();
+  T43m = compute_matrix(th3_6, 2).inverse() * compute_matrix(th2_6, 1).inverse() * compute_matrix(th1_1, 0).inverse() * T60 * compute_matrix(th6_2, 5).inverse() * compute_matrix(th5_2, 4).inverse();
   xhat43 << T43m(0, 0), T43m(1, 0), T43m(2, 0);
   double th4_6 = std::real(atan2(xhat43(1), xhat43(0)));
 
-  T43m = computeMatrix(th3_7, 2).inverse() * computeMatrix(th2_7, 1).inverse() * computeMatrix(th1_2, 0).inverse() * T60 * computeMatrix(th6_3, 5).inverse() * computeMatrix(th5_3, 4).inverse();
+  T43m = compute_matrix(th3_7, 2).inverse() * compute_matrix(th2_7, 1).inverse() * compute_matrix(th1_2, 0).inverse() * T60 * compute_matrix(th6_3, 5).inverse() * compute_matrix(th5_3, 4).inverse();
   xhat43 << T43m(0, 0), T43m(1, 0), T43m(2, 0);
   double th4_7 = std::real(atan2(xhat43(1), xhat43(0)));
 
-  T43m = computeMatrix(th3_8, 2).inverse() * computeMatrix(th2_8, 1).inverse() * computeMatrix(th1_2, 0).inverse() * T60 * computeMatrix(th6_4, 5).inverse() * computeMatrix(th5_4, 4).inverse();
+  T43m = compute_matrix(th3_8, 2).inverse() * compute_matrix(th2_8, 1).inverse() * compute_matrix(th1_2, 0).inverse() * T60 * compute_matrix(th6_4, 5).inverse() * compute_matrix(th5_4, 4).inverse();
   xhat43 << T43m(0, 0), T43m(1, 0), T43m(2, 0);
   double th4_8 = std::real(atan2(xhat43(1), xhat43(0)));
 
@@ -270,15 +251,6 @@ const Eigen::MatrixXd KIN::ik(const Eigen::Matrix4d &T60) {
       th1_1, th2_6, th3_6, th4_6, th5_2, th6_2,
       th1_2, th2_7, th3_7, th4_7, th5_3, th6_3,
       th1_2, th2_8, th3_8, th4_8, th5_4, th6_4;
-
-      //std::cout << get_position(fk(Th.row(0))).transpose() << std::endl;
-      //std::cout << get_position(fk(Th.row(1))).transpose() << std::endl;
-      //std::cout << get_position(fk(Th.row(2))).transpose() << std::endl;
-      //std::cout << get_position(fk(Th.row(3))).transpose() << std::endl;
-      //std::cout << get_position(fk(Th.row(4))).transpose() << std::endl;
-      //std::cout << get_position(fk(Th.row(5))).transpose() << std::endl;
-      //std::cout << get_position(fk(Th.row(6))).transpose() << std::endl;
-      //std::cout << get_position(fk(Th.row(7))).transpose() << std::endl;
 
   return Th;
 }
