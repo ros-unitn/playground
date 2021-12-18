2f2c5ead352dd6dcf90264dcd3b12e483c19a058
Filippo Rossi <12383260+notfilippo@users.noreply.github.com>
2021-12-18 16:05:45 +0100

    fix: same hight robot base + better placed camera (#3)

--- src/f_yolo/scripts/yolo.py
+++ src/f_yolo/scripts/yolo.py
@@ -1,15 +1,19 @@
 #! /usr/bin/env python3
 
-import os
 import cv2
 import torch
-import torch.backends.cudnn as cudnn
 import numpy as np
+
+from pathlib import Path
 
 import rospy
 from sensor_msgs.msg import Image
 
-file_path = os.path.realpath(__file__)
+file_path = Path(__file__)
+package_path = file_path.parents[1]
+
+yolo_repo_path = Path(package_path, "yolov5")
+yolo_weights_path = Path(package_path, "yolo.pt")
 
 def image_callback_1(image):
     print(image.height, image.width)
@@ -22,7 +26,7 @@ def image_callback_1(image):
 
 if __name__ == '__main__':
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = torch.hub.load('/home/ros/Workspace/src/ros_yolo/yolov5-master', 'custom', path='/home/ros/Workspace/src/ros_yolo/last.pt', source='local')  # local repo
+    model = torch.hub.load(yolo_repo_path, 'custom', path=yolo_weights_path, source='local')  # local repo
 
     rospy.init_node('ros_yolo')
     rospy.Subscriber("/camera/color/image_raw", Image, image_callback_1, queue_size=1, buff_size=52428800)
